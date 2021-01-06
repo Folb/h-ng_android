6b722098a47b3caa0412dc9cd7ffd598c7e3d785
folb <oys.follo@gmail.com>
2021-01-06 13:30:30 +0100

    :wrench: Navigation Component, Safe args, DataBinding

---
+++ app/src/main/java/io/folb/h_ng/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.folb.h_ng
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
